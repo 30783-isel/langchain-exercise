ac278e080d7063e90df54e506be074b1146e7b4d
Paulo Estrela <janganga@gmail.com>
2026-01-17 23:13:50 +0000

    teste

---
+++ git-commit-push.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM Verifica se a mensagem foi passada
+IF "%~1"=="" (
+    echo Usa: git-commit-push "mensagem do commit"
+    exit /b 1
+)
+
+REM Mostra a mensagem
+echo Mensagem do commit: %~1
+
+REM Adiciona ficheiros
+git add .
+
+REM Faz commit
+git commit -m "%~1"
+
+REM Faz push
+git push
+
+echo ---
+echo Operacao concluida!
